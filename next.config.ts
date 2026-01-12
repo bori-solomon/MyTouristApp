1400f37280fa76b7e620eae3ca26f663f56d76ea
Bori Solomon <bori.solomon@aquarius-spectrum.com>
2026-01-12 18:27:26 +0200

    Update allowedOrigins in next.config.ts for Vercel deployment

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,10 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   experimental: {
     serverActions: {
-      allowedOrigins: ["2e8d29565f30b72f-199-203-64-245.serveousercontent.com", "localhost:3000"]
+      allowedOrigins: [
+        "bs-tourist-app.vercel.app",
+        "localhost:3000"
+      ]
     }
   }
 };
